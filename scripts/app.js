cd5ced50063d92eb9ecb6ff3ceec5acd0cd9f0a5
Jeff Lueck <lueckcfd@yahoo.com>
2016-10-26 21:46:54 -0500

    edit app.js file

--- scripts/app.js
+++ scripts/app.js
@@ -13,23 +13,23 @@
         url: '/',
         templateUrl: '/templates/home.html'
       });
-
+    $stateProvider
       .state('list', {
         url: '/list.html',
-        templateUrl: '/templates/list.html'
+        templateUrl: '/templates/list.html',
       });
-
+    $stateProvider
       .state('new-entry', {
         url: '/new-entry.html',
-        templateUrl: '/templates/new-entry.html'
+        templateUrl: '/templates/new-entry.html',
       });
-      
+    $stateProvider
       .state('single', {
         url: '/single.html',
         templateUrl: '/templates/single.html'
       });
 
-  }
+}
 
   angular.module("cmsa", ['ui.router'])
          .config(config);
