4ad32546dfb8f50eaa845b52a2d805f8e407c541
Jeff Lueck <lueckcfd@yahoo.com>
2016-11-23 09:13:32 -0600

    addded new files and started backend

--- scripts/controllers/NewCtrl.js
+++ scripts/controllers/NewCtrl.js
@@ -36,26 +36,8 @@
       return newStudent;
       $scope.students.push(newStudent);
     }
-
-    // this.heading = "New Student";
-
-    this.new = [
-      {
-        studentsName: "Students Name",
-        parentsName: "Parents Name",
-        address: "Home Address",
-        phoneNumber: "Phone Number",
-        email: "Email",
-        birthday: "Birthday",
-        school: "School",
-        grade: "Grade in School",
-        years: "Years in Lessons",
-        otherInstruments: "Other Instruments",
-        reasonLessons: "Why are you intrested in taking lessons",
-        goals: "What are your goals"
-      }
-    ];
-}
+    
+};
 
 
 
