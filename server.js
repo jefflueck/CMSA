52f704d54ad39f661e668d24b79f4939f22a20f5
Jeff Lueck <lueckcfd@yahoo.com>
2016-11-30 09:03:18 -0600

    new file

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello world!');
+});
+
+app.get('/about', function(req, res) {
+  res.send('About');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
